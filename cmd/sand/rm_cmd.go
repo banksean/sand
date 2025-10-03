be878b709fde4e48b99ec17515d2ea23c96a3a84
Sean McCullough <banksean@gmail.com>
2025-10-03 15:14:30 -0700

    add -all to rm cmd

--- cmd/sand/rm_cmd.go
+++ cmd/sand/rm_cmd.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 	"os"
 
@@ -9,12 +10,14 @@ import (
 )
 
 type RmCmd struct {
-	ID string `arg:"" optional:"" help:"ID of the sandbox to remove"`
+	ID  string `arg:"" optional:"" help:"ID of the sandbox to remove"`
+	All bool   `help:"remove all sandboxes"`
 }
 
 func (rm *RmCmd) Run(cctx *Context) error {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
+	slog.InfoContext(ctx, "RmCmd", "run", *rm)
 
 	sber := sandbox.NewSandBoxer(
 		cctx.CloneRoot,
@@ -25,16 +28,32 @@ func (rm *RmCmd) Run(cctx *Context) error {
 		slog.ErrorContext(ctx, "os.Getwd", "error", err)
 		return err
 	}
-	slog.InfoContext(ctx, "LsCmd.Run", "sber", sber, "cwd", cwd)
-	sbx, err := sber.Get(ctx, rm.ID)
-	if err != nil {
-		return err
+	ids := []string{}
+	if !rm.All {
+		ids = append(ids, rm.ID)
+	} else {
+		bxs, err := sber.List(ctx)
+		if err != nil {
+			return err
+		}
+		for _, bx := range bxs {
+			ids = append(ids, bx.ID)
+		}
 	}
-	if sbx == nil {
-		return nil
-	}
-	if err := sber.Cleanup(ctx, sbx); err != nil {
-		return err
+
+	slog.InfoContext(ctx, "RmCmd.Run", "sber", sber, "cwd", cwd)
+	for _, id := range ids {
+		sbx, err := sber.Get(ctx, id)
+		if err != nil {
+			return err
+		}
+		if sbx == nil {
+			return nil
+		}
+		if err := sber.Cleanup(ctx, sbx); err != nil {
+			return err
+		}
+		fmt.Printf("%s\n", id)
 	}
 
 	return nil
