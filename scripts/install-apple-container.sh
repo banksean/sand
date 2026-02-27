5a7c29605ad0587290cd50e5bb2632c8aa73f098
Sean McCullough <banksean@gmail.com>
2026-02-27 14:05:27 -0800

    rev apple/container to 0.10.0

--- scripts/install-apple-container.sh
+++ scripts/install-apple-container.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
+CONTAINER_PKG_VERSION=0.10.0
 
-curl -L "https://github.com/apple/container/releases/download/0.9.0/container-installer-signed.pkg" -o container-installer-signed.pkg
-sudo installer -pkg container-installer-signed.pkg -target /
+echo "fetching container-$CONTAINER_PKG_VERSION-installer-signed.pkg..." 
+curl -L "https://github.com/apple/container/releases/download/$CONTAINER_PKG_VERSION/container-$CONTAINER_PKG_VERSION-installer-signed.pkg" -o /tmp/container-$CONTAINER_PKG_VERSION-installer-signed.pkg
+
+which -s container && echo "stopping container system" && container system stop
+
+sudo installer -pkg /tmp/container-$CONTAINER_PKG_VERSION-installer-signed.pkg -target /
 pkgutil --only-files --files com.apple.container-installer
+container system start
